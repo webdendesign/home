bb38ec44aa10a58940f45da5b891fb7d370b8e1b
Den <fabri.din@yandex.ru>
2021-04-08 16:25:49 +0500

    begin components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Hello, world!`;
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const Settings = {
+  AMOUNT: 3
+};
+
+ReactDOM.render(<App placesAmount={Settings.AMOUNT} />, document.querySelector(`#root`));
